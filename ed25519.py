aba52f869ed3bc22f831be709c9d740672bc1068
stasbar <stasbar1995@gmail.com>
2020-02-29 02:11:21 +0100

    add type annotations

--- ed25519.py
+++ ed25519.py
@@ -2,33 +2,33 @@ import hashlib
 import random
 
 
-def sha512(s):
+def sha512(s: bytes) -> bytes:
     return hashlib.sha512(s).digest()
 
 
 # Base field Z_p
-p = 2**255 - 19
+p: int = 2**255 - 19
 
 
-def modp_inv(x):
+def modp_inv(x) -> int:
     return pow(x, p-2, p)
 
 
 # Curve constant
-d = -121665 * modp_inv(121666) % p
+d: int = -121665 * modp_inv(121666) % p
 
 # Group order
-q = 2**252 + 27742317777372353535851937790883648493
+q: int = 2**252 + 27742317777372353535851937790883648493
 
 
-def sha512_modq(s):
+def sha512_modq(s) -> int:
     return int.from_bytes(sha512(s), "little") % q
 
 
 # Points are represented as tuples (X, Y, Z, T) of extended coordinates,
 # with x = X/Z, y = Y/Z, x*y = T/Z
 
-def point_add(P, Q):
+def point_add(P: tuple, Q: tuple) -> tuple:
     A = (P[1]-P[0])*(Q[1]-Q[0]) % p
     B = (P[1]+P[0])*(Q[1]+Q[0]) % p
     C = 2 * P[3] * Q[3] * d % p
@@ -42,7 +42,7 @@ def point_add(P, Q):
 # Computes Q = s * Q
 
 
-def point_mul(s, P):
+def point_mul(s, P) -> tuple:
     Q = (0, 1, 1, 0)  # Neutral element
     while s > 0:
         # Is there any bit-set predicate?
@@ -63,7 +63,7 @@ def point_equal(P, Q):
 
 
 # Square root of -1
-modp_sqrt_m1 = pow(2, (p-1) // 4, p)
+modp_sqrt_m1: int = pow(2, (p-1) // 4, p)
 
 # Compute corresponding x coordinate, with low bit corresponding to
 # sign, or return None on failure
@@ -92,19 +92,19 @@ def recover_x(y, sign):
 
 
 # Base point
-g_y = 4 * modp_inv(5) % p
+g_y: int = 4 * modp_inv(5) % p
 g_x = recover_x(g_y, 0)
-G = (g_x, g_y, 1, g_x * g_y % p)
+G: tuple = (g_x, g_y, 1, g_x * g_y % p)
 
 
-def point_compress(P):
+def point_compress(P: tuple) -> bytes:
     zinv = modp_inv(P[2])
     x = P[0] * zinv % p
     y = P[1] * zinv % p
     return int.to_bytes(y | ((x & 1) << 255), 32, "little")
 
 
-def point_decompress(s):
+def point_decompress(s: bytes) -> tuple:
     if len(s) != 32:
         raise Exception("Invalid input length for decompression")
     y = int.from_bytes(s, "little")
@@ -118,7 +118,7 @@ def point_decompress(s):
         return (x, y, 1, x*y % p)
 
 
-def secret_expand(secret):
+def secret_expand(secret: bytes) -> (int, bytes):
     if len(secret) != 32:
         raise Exception("Bad size of private key")
     h = sha512(secret)
@@ -128,86 +128,89 @@ def secret_expand(secret):
     return (a, h[32:])
 
 
-def secret_to_public(secret):
+def secret_to_public(secret: bytes) -> bytes:
     (a, dummy) = secret_expand(secret)
     return point_compress(point_mul(a, G))
 
 
-def sign(secret, msg):
-    a, prefix = secret_expand(secret)
-    A = point_compress(point_mul(a, G))
-    r = sha512_modq(prefix + msg)  # kinda random k
-    R = point_mul(r, G)  # g^k mod p
-    Rs = point_compress(R)
-    h = sha512_modq(Rs + A + msg)
-    s = (r + h * a) % q
+def sign(secret: bytes, msg: bytes) -> bytes:
+    (a, dummy) = secret_expand(secret)
+    A: bytes = secret_to_public(secret)
+    r: int = random_int()
+    R: tuple = point_mul(r, G)  # g^k mod p
+    Rs: bytes = point_compress(R)
+    h: int = sha512_modq(Rs + msg)
+    s: int = (r + h * a) % q
     S = int.to_bytes(s, 32, "little")
     return Rs + S
 
 
-def get_k(secret, msg):
-    a, prefix = secret_expand(secret)
-    A = point_compress(point_mul(a, G))
-    r = sha512_modq(prefix + msg)
-    return r
+def get_k() -> int:
+    k = random_int()
+    return k
 
 
-def get_R(r):
-    R = point_mul(r, G)
+def get_R(k: int) -> tuple:
+    R = point_mul(k, G)
     return R
 
 
-def random_int():
+def random_int() -> int:
     return random.randint(1, q-1)
 
 
-def get_r_prim_and_e(secret, public, message, R, alpha, beta):
+def get_r_prim_and_e(secret: bytes, public: bytes, message: bytes, R: tuple, alpha: int, beta: int) -> (tuple, int):
     # r’ = r*(g^a)*(y^b) mod p
     # g^a
+    aG: tuple = point_mul(alpha, G)
 
-    aG = point_mul(alpha, G)
+    x, prefix = secret_expand(secret)
+    A: tuple = point_mul(x, G)
+    bA: tuple = point_mul(beta, A)
 
-    a, prefix = secret_expand(secret)
-    A = point_mul(a, G)
-    bA = point_mul(beta, A)
-
-    r_prim = point_add(point_add(R, aG), bA)
+    r_prim: tuple = point_add(point_add(R, aG), bA)
+    Rs: bytes = point_compress(r_prim)
     # e’ = H(M || r’)
-    e_prim = sha512_modq(point_compress(r_prim) + public + message) # point_compress not sure here
+    e_prim: int = int.from_bytes(sha512(Rs + message), "little")  # point_compress not sure here
     # e = e’ – b mod q
-    e=(e_prim - beta) % q
+    e: int = (e_prim - beta) % q
     return (r_prim, e)
 
-def get_s(e, secret, k):
+
+def get_s(e, secret: bytes, k: int) -> int:
     # s = ex + k mod q
-    a, prefix=secret_expand(secret)
-    s=(e * a + k) % q
+    x, prefix = secret_expand(secret)
+    s: int = (e * x + k) % q
     return s
 
-def get_s_prim(s, a):
+
+def get_s_prim(s: tuple, alpha: tuple) -> int:
     # s_pri = s - a mod q
-    s_pri=(s - a) % q
+    s_pri: int = (s - alpha) % q
     return s_pri
 
-def blind_sign(r_prim, s_prim):
-    Rs=point_compress(r_prim)
-    S=int.to_bytes(s_prim, 32, "little")
+
+def blind_sign(r_prim: tuple, s_prim: int) -> bytes:
+    Rs: bytes = point_compress(r_prim)
+    S: bytes = int.to_bytes(s_prim, 32, "little")
     return Rs + S
+
 
 def verify(public, msg, signature):
     if len(public) != 32:
         raise Exception("Bad public key length")
     if len(signature) != 64:
         Exception("Bad signature length")
-    A=point_decompress(public)
+    
+    A: tuple = point_decompress(public)
     if not A:
         return False
-    Rs=signature[:32]
-    R=point_decompress(Rs)
+    Rs: bytes = signature[:32]
+    R: tuple = point_decompress(Rs)
     if not R:
         return False
-    s=int.from_bytes(signature[32:], "little")
-    h=sha512_modq(Rs + public + msg)
-    sB=point_mul(s, G)
-    hA=point_mul(h, A)
+    s: int = int.from_bytes(signature[32:], "little")
+    h: int = sha512_modq(Rs + msg)
+    sB: tuple = point_mul(s, G)
+    hA: tuple = point_mul(h, A)
     return point_equal(sB, point_add(R, hA))
--- main.py
+++ main.py
@@ -7,22 +7,22 @@ public = "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a"
 message = ""
 signature = "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b"
 
-secret_bin = (binascii.unhexlify(secret))[:32]
-public_bin = binascii.unhexlify(public)
-message_bin = binascii.unhexlify(message)
+secret_bin: bytes = (binascii.unhexlify(secret))[:32]
+public_bin: bytes = binascii.unhexlify(public)
+message_bin: bytes = binascii.unhexlify(message)
 
-sign = ed25519.sign(secret_bin, message_bin)
+sign: bytes = ed25519.sign(secret_bin, message_bin)
 print(ed25519.verify(public_bin, message_bin, sign))
 
 # Blind signatre
-k = ed25519.get_k(secret_bin, message_bin)
-R = ed25519.get_R(k)
-a = ed25519.random_int()
-b = ed25519.random_int()
-(r_prim, e) = ed25519.get_r_prim_and_e(secret_bin, public_bin, message_bin, R, a, b)
-s = ed25519.get_s(e, secret_bin, k)
-s_prim = ed25519.get_s_prim(s, a)
+k: int = ed25519.get_k()
+R: tuple = ed25519.get_R(k)
+a: int = ed25519.random_int()
+b: int = ed25519.random_int()
+r_prim, e = ed25519.get_r_prim_and_e(secret_bin, public_bin, message_bin, R, a, b)
+s: int = ed25519.get_s(e, secret_bin, k)
+s_prim: int = ed25519.get_s_prim(s, a)
 
-blind_sign = ed25519.blind_sign(r_prim, s_prim)
+blind_sign: bytes = ed25519.blind_sign(r_prim, s_prim)
 
 print(ed25519.verify(public_bin, message_bin, blind_sign))
